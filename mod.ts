619d58b369fa5a458aa29608308691769eb61943
Satnam Singh <satnam.singh156@t-mobile.com>
2020-06-02 11:03:07 +0530

    Intiall commit

---
+++ mod.ts
@@ -0,0 +1,63 @@
+
+
+function makePrefix(key: any, last: any) {
+    let str = (last ? '└' : '├');
+    if (key) {
+        str += '─ ';
+    } else {
+        str += '──┐';
+    }
+    return str;
+}
+
+function filterKeys(obj: any, hideFunctions: any) {
+    let keys = [];
+    for (let branch in obj) {
+        if (!obj.hasOwnProperty(branch)) {
+            continue;
+        }
+        if (hideFunctions && ((typeof obj[branch]) === "function")) {
+            continue;
+        }
+        keys.push(branch);
+    }
+    return keys;
+}
+
+function growBranch(key: any, root: any, last: any, lastStates: any, showValues: any, hideFunctions: any, callback: any) {
+    let line = '', index = 0, lastKey, circular: any, lastStatesCopy = lastStates.slice(0);
+
+    if (lastStatesCopy.push([root, last]) && lastStates.length > 0) {
+        lastStates.forEach(function (lastState: any, idx: any) {
+            if (idx > 0) {
+                line += (lastState[1] ? ' ' : '│') + '  ';
+            }
+            if (!circular && lastState[0] === root) {
+                circular = true;
+            }
+        });
+        line += makePrefix(key, last) + key;
+        showValues && (typeof root !== 'object' || root instanceof Date) && (line += ': ' + root);
+        circular && (line += ' (circular ref.)');
+
+        callback(line);
+    }
+
+    if (!circular && typeof root === 'object') {
+        let keys = filterKeys(root, hideFunctions);
+        keys.forEach(function (branch) {
+            lastKey = ++index === keys.length;
+            growBranch(branch, root[branch], lastKey, lastStatesCopy, showValues, hideFunctions, callback);
+        });
+    }
+};
+
+let JsonTree = function (obj: any, showValues: any, hideFunctions?: any) {
+    let tree = '';
+    growBranch('.', obj, false, [], showValues, hideFunctions, function (line: any) {
+        tree += line + '\n';
+    });
+    return tree;
+};
+
+export {JsonTree};
---
+++ readme.md
@@ -0,0 +1,53 @@
+# JSON Tree 
+
+Json Tree converts a JS object into a nice, visible depth-indented tree for console printing. The structure
+generated is similar to what you get by running the ```tree``` command on Unixy platforms.
+
+```
+{
+    oranges: {
+        'mandarin': {                                          ├─ oranges
+            clementine: null,                                  │  └─ mandarin
+            tangerine: 'so cheap and juicy!'        -=>        │     ├─ clementine
+        }                                                      │     └─ tangerine: so cheap and juicy!
+    },                                                         └─ apples
+    apples: {                                                     ├─ gala
+        'gala': null,                                             └─ pink lady
+        'pink lady': null
+    }
+}
+```
+
+It also works well with larger nested hierarchies such as file system directory trees.
+In fact, the ```fs_tree``` example does a pretty good job of imitating ```tree```. Try it out!
+
+
+Getting it
+----------
+
+### For use with deno
+
+```ts
+import { JsonTree } from "https://deno.land/x/JsonTree/mod.ts";
+console.log(
+   JsonTree({
+      apples: 'gala',      //  ├─ apples: gala
+      oranges: 'mandarin'  //  └─ oranges: mandarin
+   }, true)
+);
+```
+
+Usage
+-----
+
+The methods exposed to you are as follows, in a strange kind of signature notation:
+
+
+### JsonTree()
+```js
+JsonTree(obj, showValues (boolean), hideFunctions:bollean)
+
+obj : Json Object
+showValues : To Show the object values in tree. set it true or false
+hideFunctions : To Show the function in tree. set it true or false
+```
